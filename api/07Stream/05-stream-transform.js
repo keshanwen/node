15e937fe34b88473868d47fefd387b94f679b1fa
keshanwen <3640@winnermedical.com>
2023-06-01 10:10:33 +0800

    feat流

---
+++ api/07Stream/05-stream-transform.js
@@ -0,0 +1,21 @@
+let {Transform} = require('stream')
+
+
+class MyTransform extends Transform {
+  constructor() {
+    super()
+  }
+
+  _transform(chunk, en, cb) {
+    this.push(chunk.toString().toUpperCase())
+    cb(null)
+  }
+}
+
+let t = new MyTransform()
+
+t.write('hello wrold')
+
+t.on('data', (chunk) => {
+  console.log(chunk.toString())
+})
