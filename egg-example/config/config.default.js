4fac2ce6b7bc38b713371b03257a80f27eab7a4d
keshanwen <3640@winnermedical.com>
2023-08-03 15:56:47 +0800

    feat: 快速入门

---
+++ egg-example/config/config.default.js
@@ -0,0 +1,24 @@
+// config/config.default.js
+exports.keys = 'kebi';
+
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+};
+
+// config/config.default.js
+// 添加 news 的配置项
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+
+// config/config.default.js
+// add middleware robot
+exports.middleware = ['robot'];
+// robot's configurations
+exports.robot = {
+  ua: [/Baiduspider/i],
+};
---
+++ egg-example/config/plugin.js
@@ -0,0 +1,6 @@
+// config/plugin.js
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks',
+};
+
